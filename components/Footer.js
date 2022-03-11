e926454d717638f1c76fbd1d1725c4c203748c7f
Viviana Davila <vdavila@ucsd.edu>
2022-03-10 19:50:31 -0800

    update header

--- components/Footer.js
+++ components/Footer.js
@@ -1,16 +1,28 @@
 import Link from "next/link";
 
 export default function Footer() {
-    return (
-        <footer className="animate__animated animate__fadeIn animate__delay-2s">
-          <div className="container">
-            <ul>
-              <li><Link href="mailto:vividavila98@gmail.com">Email</Link></li>
-              <li><Link href="/">Resume</Link></li>
-              <li><Link href="https://github.com/vividavila98">Github</Link></li>
-              <li><Link href="https://www.linkedin.com/in/viviana-davila/">LinkedIn</Link></li>
-            </ul>
-          </div>
-        </footer>
-    )
+  return (
+    <footer className="animate__animated animate__fadeIn animate__delay-2s">
+      <div className="container">
+        <ul>
+          <li>
+            <Link href="mailto:vividavila98@gmail.com">Email</Link>
+          </li>
+          <li>
+            <Link href="./assets/Viviana_Davila.pdf" rel="noopener noreferrer">
+              Resume
+            </Link>
+          </li>
+          <li>
+            <Link href="https://github.com/vividavila98">Github</Link>
+          </li>
+          <li>
+            <Link href="https://www.linkedin.com/in/viviana-davila/">
+              LinkedIn
+            </Link>
+          </li>
+        </ul>
+      </div>
+    </footer>
+  );
 }
--- components/Header.js
+++ components/Header.js
@@ -1,30 +1,44 @@
-import React from 'react';
-import Head from "next/head"; 
+import React from "react";
+import Head from "next/head";
 import Image from "next/image";
-import realPic from '../public/images/grad-pic.jpg';
+import realPic from "../public/images/grad-pic.jpg";
 
 export default function Header() {
-    return (
-        <>
-        <Head>
-          <title>Viviana Davila | Los Angeles, CA </title>
-          <meta name="description" content="Web Dev Blog" />
-          <link rel="icon" href="/favicon.ico" />
-        </Head>
-        <header className="header">
-          <div className="introBox animate__animated animate__fadeIn animate__delay-1s">
-            <h1 className="title">Viviana Davila</h1>
-            <div className="codeText">Front-End Developer</div>
-            <p className="intro">Hello! I’m a Front-End Developer based in Los Angeles, CA 
-            with an emphasis on utilizing <span className="code">ReactJS</span> to build both simple mobile-responsive websites & 
-            dynamic web applications.</p>
-          </div>
-          
-          <div className="picBox animate__animated animate__fadeIn animate__delay-1s">
-            <Image src={realPic} className="topPic" width={500} height={500} alt="Picture of me" loading="eager"/>
-            <div className="picOutline"></div>
-          </div>
-        </header>
-        </>
-    )
+  return (
+    <>
+      <Head>
+        <title>Viviana Davila</title>
+        <meta name="description" content="Web Dev Blog" />
+        <meta
+          name="image"
+          property="og:image"
+          content="./images/portfolio.png"
+        />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <header className="header">
+        <div className="introBox animate__animated animate__fadeIn animate__delay-1s">
+          <h1 className="title">Viviana Davila</h1>
+          <div className="codeText">Frontend Developer</div>
+          <p className="intro">
+            Hello! I’m a Frontend Developer based in Tulare, CA with an emphasis
+            on utilizing <span className="code">ReactJS</span> to build both
+            simple mobile-responsive websites & dynamic web applications.
+          </p>
+        </div>
+
+        <div className="picBox animate__animated animate__fadeIn animate__delay-1s">
+          <Image
+            src={realPic}
+            className="topPic"
+            width={500}
+            height={500}
+            alt="Picture of me"
+            loading="eager"
+          />
+          <div className="picOutline"></div>
+        </div>
+      </header>
+    </>
+  );
 }
